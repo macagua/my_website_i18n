60ebea5a44ba401344cc46ce7f397d6776d3de11
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-12-01 05:22:58 -0400

    Added i18n extra file for Spanish translation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # my_website_i18n
+
 An example for override the default Odoo translations.
+
+The following strings were changes via GNU Gettext editor:
+
+* Odoo ---> Macaguax
+
+* https://www.odoo.com ---> https://macagua.github.io/
+
---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Leonardo Caballero <leonardocaballero@gmail.com>
+{
+    'name': "My Website i18n",
+    'summary': "My Website i18n customizations",
+    'description': """
+Load My Website i18n customizations.
+====================================
+
+Allows to automatically load data and set the following item:
+--------------------------------------------------------------
+    * Add i18n override support for customs translations string.
+    """,
+    'author': 'Leonardo Caballero <leonardocaballero@gmail.com>',
+    'website': "https://github.com/macagua/my_website_i18n",
+    'maintainer': "Leonardo Caballero <leonardocaballero@gmail.com>",
+    'license': "Other proprietary",
+    'category': 'Localization/Language',
+    'version': '0.1',
+    'depends': ['website'],
+    'post_load': None,
+    'application': False,
+    'auto_install': False,
+    'installable': True,
+}
+
